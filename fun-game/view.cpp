33d08881acab280cbffcd224d850e67bc120281a
GozSoft <goverclock@outlook.com>
2022-07-14 12:26:30 +0800

    gui basic

--- fun-game/view.cpp
+++ fun-game/view.cpp
@@ -1,9 +1,34 @@
 #include "view.h"
 
+#include "gui/button.h"
+
 View::View() {
     setFixedSize(window_w, window_h);
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setScene(&sce);
+    setSceneRect(0, 0, window_w, window_h);
+
+    init();
 
     show();
+}
+
+void View::init() {
+    bt = new Button("好");
+    bt->set_mode(Button::normal);
+    bt->setPos(0, 0);
+    bt->on(sce);
+    connect(bt, &Button::clicked, [] { qDebug("fuck"); });
+
+    line = new QGraphicsLineItem();
+    line->setLine(200, 0, 200, 480);
+    sce.addItem(line);
+}
+
+bool View::eventFilter(QObject *obj, QEvent *e) {
+    if (e->type() == QEvent::Wheel)
+        return true;
+    else
+        return QObject::eventFilter(obj, e);
 }
--- fun-game/view.h
+++ fun-game/view.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <QGraphicsScene>
 #include <QGraphicsView>
+#include <QGraphicsLineItem>
+#include "gui/button.h"
 
 class View : public QGraphicsView {
     Q_OBJECT
@@ -11,4 +14,13 @@ class View : public QGraphicsView {
     const int window_w = 800;
     const int window_h = 480;
 
+    QGraphicsScene sce;
+
+    // gui
+    QGraphicsLineItem *line;
+    Button *bt;
+
+    void init();
+
+    bool eventFilter(QObject *, QEvent *);
 };
